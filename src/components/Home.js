07e37ede14b3faac3f3d282d29b363040fd224ca
rickyjoe11 <48981536+rickyjoe11@users.noreply.github.com>
2023-03-24 12:04:29 -0400

    Added new components, modified existing components

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,10 @@ import Hero from './Hero'
 import Booking from './Booking'
 import HomeFleet from './HomeFleet'
 import WhyUs from './WhyUs'
+import Testimonials from './Testimonials'
+import FAQ from './FAQ'
+import DownloadApp from './DownloadApp'
+import Footer from './Footer'
 
 function Home() {
   return (
@@ -38,6 +42,9 @@ function Home() {
     </div>
     <HomeFleet />
     <WhyUs />
+    <Testimonials />
+    <FAQ />
+    <DownloadApp />
     </>
   )
 }
